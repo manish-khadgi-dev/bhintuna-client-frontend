a43db31e32dfba10dec9bc6c69a52084e3db4c79
moonishkale <manishkhadgi123@gmail.com>
2023-03-27 12:17:20 +1100

    ff

--- src/pages/menu-cards/MenuCards.js
+++ src/pages/menu-cards/MenuCards.js
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/esm/Container";
 import momo2 from "../../assets/momo2.jpg";
@@ -6,62 +6,50 @@ import momo2 from "../../assets/momo2.jpg";
 function MenuCards() {
   return (
     <Container className="mt-5 p-5">
-      <Row className="gap-5 d-flex justify-content-between">
-        <Col>
-          <Card style={{ width: "18rem", height: "30rem" }}>
-            <Card.Img variant="top" src={momo2} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+      <Row className="d-flex justify-content-center gap-5">
+        <Card style={{ width: "20rem", height: "29rem" }}>
+          <Card.Img variant="top" src={momo2} />
+          <Card.Body>
+            <Card.Title>Chicken Momo </Card.Title>
+            <Card.Text></Card.Text>
+            <Button variant="primary">Add </Button>
+          </Card.Body>
+        </Card>
 
-        <Col>
-          <Card style={{ width: "18rem", height: "30rem" }}>
-            <Card.Img variant="top" src={momo2} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Card style={{ width: "20rem", height: "29rem" }}>
+          <Card.Img variant="top" src={momo2} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
 
-        <Col>
-          <Card style={{ width: "18rem", height: "30rem" }}>
-            <Card.Img variant="top" src={momo2} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card style={{ width: "18rem", height: "30rem" }}>
-            <Card.Img variant="top" src={momo2} />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Card style={{ width: "20rem", height: "29rem" }}>
+          <Card.Img variant="top" src={momo2} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "20rem", height: "29rem" }}>
+          <Card.Img variant="top" src={momo2} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </Row>
     </Container>
   );
